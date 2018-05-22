a2b11ab07f62a38f85b3b701356dcaf5fed7f349
Justin Heyes-Jones <justinhj.iggca@gmail.com>
2018-05-22 10:30:34 -0700

    Completed example of batched jobs

--- src/main/scala/examples/QueueSample.scala
+++ src/main/scala/examples/QueueSample.scala
@@ -1,7 +1,8 @@
 package examples
 
 import scala.util.Random
-import java.util.concurrent.ConcurrentLinkedQueue
+import java.util.concurrent.{ConcurrentLinkedQueue, CountDownLatch}
+
 import scala.concurrent.duration._
 import monix.execution.Cancelable
 import monix.reactive.Observable
@@ -9,22 +10,36 @@ import monix.execution.Scheduler.Implicits.global
 
 object QueueSample {
 
+  // This is a pretend job processor
+
   val r = Random
 
+  // Job with a unique Id and a name
+  // This would be the full job details, for example
+  // in a DB writer this would be the data to write, the ID
+  // and possibly a callback
   case class Job(name: String, id: Long)
 
+  // Job queue is a concurrent linked queue
   val jobs = new ConcurrentLinkedQueue[Job]()
 
   def makeAJob(n : Long) = {
     Job(s"Job $n", n.hashCode().toLong)
   }
 
-  def startJobs() : Cancelable = {
+  val testJobs = 500 // how many jobs to process before shutting down
 
-    Observable.intervalAtFixedRate(1 second, 500 milliseconds).filter{
+  // create limit jobs over a period
+  def startJobs(limit: Int) : Cancelable = {
+
+    Observable.intervalAtFixedRate(1 second, 10 milliseconds)
+    .filter{
       x =>
+        // Randomly filter half the jobs to create some jitter
         r.nextBoolean()
-    }.map {
+    }
+    .take(limit)
+    .map {
       n => makeAJob(n)
     }//.dump("job")
     .foreach {
@@ -36,7 +51,7 @@ object QueueSample {
 
   }
 
-  // We're gonna print the queue size, remove a batch of jobs
+  // An Observable that periodically batches jobs from the concurrent queue
 
   def jobsObservable(batchSize: Integer, interval: FiniteDuration)  = {
 
@@ -62,17 +77,18 @@ object QueueSample {
 
   }
 
-  def processJobs()= {
+  def processJobs(latch: CountDownLatch)= {
 
-    jobsObservable(10, 200 milliseconds).map{
+    jobsObservable(batchSize = 10, 200 milliseconds).map{
       batch =>
         if(batch.size > 0) {
-          s"BEGIN ${batch.size} jobs starting at ${batch(0).id}"
-
           // do pretend work
           Thread.sleep(200)
 
-          s"END ${batch.size} jobs starting at ${batch(0).id}"
+          (1 to batch.size).foreach {_ => latch.countDown()}
+
+          s"Completed ${batch.size} jobs, starting at job ID ${batch(0).id}"
+
         }
         else
           "no jobs"
@@ -84,16 +100,16 @@ object QueueSample {
 
   def main(args: Array[String]): Unit = {
 
-    println("Beginning with amazing queue thing")
+    println("Beginning amazing queue thing")
 
+    val latch = new CountDownLatch(testJobs)
 
+    startJobs(testJobs) // make some jobs start happening to a public concurrent queue
 
+    processJobs(latch) // process the jobs
 
-
+    latch.await()
 
   }
-
-
-
 
 }
