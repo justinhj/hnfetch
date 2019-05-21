e34aaa9e8b55487f4fda459c83dc513792b34ad7
Justin Heyes-Jones <justinhj@gmail.com>
2019-05-21 09:12:55 -0700

    Example of divide in contramap

--- src/main/scala/examples/ContravariantMap.scala
+++ src/main/scala/examples/ContravariantMap.scala
@@ -13,11 +13,21 @@ object ContravariantMap {
   // This implements a contravariant for type Printable (providing the contramap)
   // Contramap let's us map over an A when we have a B and a function to map B to A
   object Printable {
-    implicit val contraPrintable = new Contravariant[Printable] {
+    implicit val contraPrintable = new Divide[Printable] {
       def contramap[A, B](fa: Printable[A])(f: B => A): Printable[B] = new Printable[B] {
         def format(b: B): String =
           fa.format(f(b))
       }
+
+      def divide2[A1, A2, Z](fa1: =>Printable[A1], fa2: =>Printable[A2])(f: Z => (A1, A2)): Printable[Z] = new Printable[Z] {
+        def format(z: Z): String = {
+          val (a1,a2) = f(z)
+          val a1f = fa1.format(a1)
+          val a2f = fa2.format(a2)
+          s"Divided! $a1f $a2f"
+        }
+      }
+
     }
   }
 
@@ -40,16 +50,26 @@ object ContravariantMap {
     new Printable[Int] {
       def format(value: Int): String =
         s"Int! >$value<"
-    }
+    }  
 
+  // Example for contramap
   final case class Box[A](value: A)
 
+  // Example for divide needs two things...
+  final case class TwoBox[A,B](a: A, b: B)
+
+  // Generic printables for boxes 
   implicit def boxPrintable[A](implicit p: Printable[A]): Printable[Box[A]] =
     p.contramap(_.value)
+
+  implicit def twoBoxPrintable[A,B](implicit p: Printable[A], q: Printable[B]): Printable[TwoBox[A,B]] =
+    Printable.contraPrintable.divide2(p,q)(twoBox => (twoBox.a, twoBox.b))
 
   def main(args: Array[String]): Unit = {
     println(format(Box("Hello, World!")))
     println(format(Box(true)))
     println(format(Box(3117)))
+    println(format(TwoBox("Justin", 1871)))
+    println(format(TwoBox(false, "Jimmy")))
   }
 }
